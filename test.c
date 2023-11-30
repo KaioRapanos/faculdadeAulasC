93a5e4fd17957f560a99ee851019a59f605e63ca
KaioRapanos <kaiorapanos@gmail.com>
2023-11-30 19:04:55 -0300

    Teste

--- test.c
+++ test.c
@@ -1,16 +1,17 @@
 
-// #include <stdio.h>
-// #include <string.h>
-// int main()
-// {
-//             char A[7] = {'P','i','e','t','r','o','\0'};
-//             char B[8] = {'M','a','r','t','i','n','s','\0'};
-//             char C[20];
-//             strcpy(C, A);
-//             strcat(C, B);
-//             printf("Texto: \"%s\".\n", C);
-//             return (0);
-// }
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+            char A[7] = {'P','i','e','t','r','o','\0'};
+            char B[8] = {'M','a','r','t','i','n','s','\0'};
+            char C[20];
+            strcpy(C, A);
+            strcat(C, B);
+            printf("Texto: \"%s\".\n", C);
+            return (0);
+            
+}
 
 // #include <stdio.h>
 // #include <stdlib.h>
